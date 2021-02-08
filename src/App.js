a59b545f53c281bc13760f06c26f73f5d5ee80f7
Mooooooov <mateusvasconcelos4210@gmail.com>
2021-02-08 05:27:20 -0300

    Remove more files and change the layout

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import Location from "./components/Location";
-import Navbar from "./components/Navbar";
 import Text from "./components/Text";
+import NavbarImage from "./assets/images/navbar-image.svg";
 import "./reset.scss";
 import "./global.scss";
 
 function App() {
   return (
     <>
-      <Navbar />
+      <div className="navbar-container">
+        <img src={NavbarImage} alt="logo" width="120" />
+      </div>
       <div className="main-container">
         <div className="main-text">
           <Text type="title">Will It Rain Today?</Text>
-          <Text type="subtitle">No.</Text>
+          <Text type="subtitle">Nop.</Text>
           <Location />
         </div>
       </div>
