fec14a6a8c3b128c2007e8c09e7c7fd1d4d11e52
Mooooooov <mateusvasconcelos4210@gmail.com>
2021-03-03 18:26:21 -0300

    Adding screen when the user is not yet allow the button

--- src/App.js
+++ src/App.js
@@ -1,21 +1,9 @@
-import Location from "./components/Location";
-import Text from "./components/Text";
-import NavbarImage from "./assets/images/navbar-image.svg";
-import "./reset.scss";
-import "./global.scss";
+import NoLocation from "./pages/no-location";
 
 function App() {
   return (
     <>
-      <div className="main-container">
-        <div className="main-text">
-          <Text type="title">How's the weather today?</Text>
-          <div className="navbar-container">
-            <img src={NavbarImage} alt="logo" width="120" />
-          </div>
-          <Location />
-        </div>
-      </div>
+      <NoLocation />
     </>
   );
 }
