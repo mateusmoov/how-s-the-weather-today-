bfc79456c4df482ac9c2090dc563ca1e0dec1196
Mooooooov <mateusvasconcelos4210@gmail.com>
2021-03-03 17:34:36 -0300

    testando issue

--- src/App.js
+++ src/App.js
@@ -7,13 +7,12 @@ import "./global.scss";
 function App() {
   return (
     <>
-      <div className="navbar-container">
-        <img src={NavbarImage} alt="logo" width="120" />
-      </div>
       <div className="main-container">
         <div className="main-text">
-          <Text type="title">Will It Rain Today?</Text>
-          <Text type="subtitle">Nop.</Text>
+          <Text type="title">How's the weather today?</Text>
+          <div className="navbar-container">
+            <img src={NavbarImage} alt="logo" width="120" />
+          </div>
           <Location />
         </div>
       </div>
